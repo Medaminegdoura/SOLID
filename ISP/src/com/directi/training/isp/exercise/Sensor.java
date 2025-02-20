ad0e4820d36689e8cfc0ba5bc89d52d13532fd04
Mahdi Chaabane <mahdi02ch@gmail.com>
2025-02-20 22:04:22 +0100

    ISP

--- ISP/src/com/directi/training/isp/exercise/Sensor.java
+++ ISP/src/com/directi/training/isp/exercise/Sensor.java
@@ -4,11 +4,11 @@ import java.util.Random;
 
 public class Sensor
 {
-    public void register(Door door)
+    public void register(IProximitySensibleObject object)
     {
         while (true) {
             if (isPersonClose()) {
-                door.proximityCallback();
+                object.proximityCallback();
                 break;
             }
         }
